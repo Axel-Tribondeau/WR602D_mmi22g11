6750d353ad75177908810b88fdce479cec634a5a
Github MMI3 <github@mmi-troyes.fr>
2025-03-17 10:03:27 +0000

    fix:SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Annotation\Route;  // Import de Route
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;  // Import de AuthenticationUtils
+use LogicException;  // Import de LogicException
 
 class SecurityController extends AbstractController
 {
@@ -27,7 +28,6 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it ' .
-            'will be intercepted by the logout key on your firewall.');
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
