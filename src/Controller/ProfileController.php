27bd994420fb8c92a8550d9873c1c0fc8069fcdd
Github MMI3 <github@mmi-troyes.fr>
2025-03-26 17:40:21 +0000

    fix: fixing problems with register form

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -12,13 +12,11 @@ final class ProfileController extends AbstractController
     #[Route('/profile', name: 'app_profile')]
     public function index(UserInterface $user): Response
     {
-        // Récupérer l'email de l'utilisateur
         $userEmail = $user->getEmail();
 
-        // Passer l'email à la vue pour générer un lien de réinitialisation de mot de passe
         return $this->render('profile/index.html.twig', [
             'user' => $user,
-            'userEmail' => $userEmail,  // On passe l'email de l'utilisateur à la vue
+            'userEmail' => $userEmail,
         ]);
     }
 }
