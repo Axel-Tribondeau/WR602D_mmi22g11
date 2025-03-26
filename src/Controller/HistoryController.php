bf3e54d80cd973fc5b2dce970f34cad711226a95
Github MMI3 <github@mmi-troyes.fr>
2025-03-26 13:34:37 +0000

    fix(cs): historyController

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class HistoryController extends AbstractController
 {
     #[Route('/history', name: 'app_history')]
